9cb87a70660e45033b153316e0b1f7fd88affd46
JCOM127 <93507163+JCOM127@users.noreply.github.com>
2021-11-11 15:58:21 -0500

    Add files via upload

---
+++ cargo-app/src/app/services/cities.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient, HttpResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class CitiesService {
+    constructor(private http: HttpClient) { }
+
+    public getCityInfo(cityName: string): Observable<string> {
+        return this.http.get('https://samples.openweathermap.org/data/2.5/weather?q='.concat(cityName)
+        .concat(',uk&appid=aa0e4b181ec0aa8fd08bcc3095412fb7'), {responseType: 'text'});
+    }
+} 
