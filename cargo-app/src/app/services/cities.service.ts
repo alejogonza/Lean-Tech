59fcae0ede2e6387d1a4003530450c0c79eaeca2
Alejandro Gonzalez Serna <alejo.1996.2001@gmail.com>
2021-11-11 21:45:25 -0500

    [feature] fix cors

--- cargo-app/src/app/services/cities.service.ts
+++ cargo-app/src/app/services/cities.service.ts
@@ -7,7 +7,7 @@ export class CitiesService {
     constructor(private http: HttpClient) { }
 
     public getCityInfo(cityName: string): Observable<string> {
-        return this.http.get('https://samples.openweathermap.org/data/2.5/weather?q='.concat(cityName)
+        return this.http.get('/data/2.5/weather?q='.concat(cityName)
         .concat(',uk&appid=aa0e4b181ec0aa8fd08bcc3095412fb7'), {responseType: 'text'});
     }
 } 
